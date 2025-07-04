20debe08f6ed60cba928f63bb1bebc1896711d21
Сивцов Иван <irsiv2019@outlook.com>
2025-07-05 00:10:10 +0300

    Переделал древо файлов

---
+++ Abaldui/main/mainjs.js
@@ -0,0 +1,13 @@
+// window.onbeforeunload = function () {
+//     window.scrollTo(0,0);
+// }
+
+function Misha(){
+    window.scrollTo(0,870)
+    console.log('Миша')
+}
+
+function Ivan(){
+    window.scrollTo(0,1600)
+    console.log('Иван')
+}
